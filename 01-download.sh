b54940a1e6515d7aa63fbf06a38a807d5de81ab0
Yuan-Chen Cheng <yc.cheng@canonical.com>
2021-04-13 22:13:55 +0800

    steps and cudnn765 snap

---
+++ 01-download.sh
@@ -0,0 +1,1 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.9.2-Linux-x86_64.sh
---
+++ 02-init-miniconda.sh
@@ -0,0 +1,3 @@
+./Miniconda3-py38_4.9.2-Linux-x86_64.sh -b
+~/miniconda3/bin/conda init
+echo Please re-login to have shell hook works.
